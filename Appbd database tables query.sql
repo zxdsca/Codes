72aa5d90d2dd1ee93bb421b9913087ce2e61e4eb
Yaminie <125576813+zxdsca@users.noreply.github.com>
2025-11-22 18:30:26 +0530

    Add files via upload

---
+++ Appbd database tables query.sql
@@ -0,0 +1,67 @@
+CREATE TABLE `customer` (
+    `customerId` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) DEFAULT NULL,
+    `email` varchar(100) DEFAULT NULL,
+    `phoneNumber` varchar(15) DEFAULT NULL,
+    `password` varchar(100) DEFAULT NULL,
+    PRIMARY KEY (`customerId`),
+    UNIQUE KEY `email` (`email`)
+) ENGINE = InnoDB AUTO_INCREMENT = 52 DEFAULT CHARSET = latin1
+
+CREATE TABLE `medicine` (
+    `medicineId` int(11) NOT NULL AUTO_INCREMENT,
+    `pharmacyId` int(11) DEFAULT NULL,
+    `name` varchar(100) DEFAULT NULL,
+    `price` double DEFAULT NULL,
+    `description` varchar(100) DEFAULT NULL,
+    `availableQuantity` int(11) DEFAULT NULL,
+    PRIMARY KEY (`medicineId`),
+    KEY `pharmacyId` (`pharmacyId`),
+    CONSTRAINT `medicine_ibfk_1` FOREIGN KEY (`pharmacyId`) REFERENCES `pharmacy` (`pharmacyId`)
+) ENGINE = InnoDB AUTO_INCREMENT = 15 DEFAULT CHARSET = latin1
+
+CREATE TABLE `order` (
+    `orderId` int(11) NOT NULL AUTO_INCREMENT,
+    `customerId` int(11) DEFAULT NULL,
+    `pharmacyId` int(11) DEFAULT NULL,
+    `orderStatus` varchar(20) DEFAULT NULL,
+    `totalPrice` double DEFAULT NULL,
+    `deliveryAddress` varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`orderId`),
+    KEY `customerId` (`customerId`),
+    KEY `pharmacyId` (`pharmacyId`),
+    CONSTRAINT `order_ibfk_1` FOREIGN KEY (`customerId`) REFERENCES `customer` (`customerId`),
+    CONSTRAINT `order_ibfk_2` FOREIGN KEY (`pharmacyId`) REFERENCES `pharmacy` (`pharmacyId`)
+) ENGINE = InnoDB AUTO_INCREMENT = 14 DEFAULT CHARSET = latin1
+
+CREATE TABLE `orderItem` (
+    `orderItem` int(11) NOT NULL AUTO_INCREMENT,
+    `orderId` int(11) DEFAULT NULL,
+    `medicineId` int(11) DEFAULT NULL,
+    `quantity` int(11) DEFAULT NULL,
+    PRIMARY KEY (`orderItem`),
+    KEY `orderId` (`orderId`),
+    KEY `medicineId` (`medicineId`),
+    CONSTRAINT `orderItem_ibfk_1` FOREIGN KEY (`orderId`) REFERENCES `order` (`orderId`),
+    CONSTRAINT `orderItem_ibfk_2` FOREIGN KEY (`medicineId`) REFERENCES `medicine` (`medicineId`)
+) ENGINE = InnoDB AUTO_INCREMENT = 14 DEFAULT CHARSET = latin1
+
+CREATE TABLE `payment` (
+    `paymentId` int(11) NOT NULL AUTO_INCREMENT,
+    `orderId` int(11) DEFAULT NULL,
+    `amountPaid` double DEFAULT NULL,
+    `paymentDate` date DEFAULT NULL,
+    `paymentStatus` varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`paymentId`),
+    KEY `orderId` (`orderId`),
+    CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`orderId`) REFERENCES `order` (`orderId`)
+) ENGINE = InnoDB AUTO_INCREMENT = 13 DEFAULT CHARSET = latin1
+
+CREATE TABLE `pharmacy` (
+    `pharmacyId` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) DEFAULT NULL,
+    `address` varchar(255) DEFAULT NULL,
+    `licenseNumber` varchar(25) DEFAULT NULL,
+    `contactNumber` varchar(25) DEFAULT NULL,
+    PRIMARY KEY (`pharmacyId`)
+) ENGINE = InnoDB AUTO_INCREMENT = 18 DEFAULT CHARSET = latin1
